e3cca90e3bdca759d95891be8262db4d3a0d63ce
Ryan <RyanCTaylor95@gmail.com>
2019-07-14 22:12:59 +1000

    File uploading test

---
+++ W3/express_file_upload/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const express = require('express')
+const formidable = require('formidable')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.parse(req)
+    form.on('fileBegin', (name, file) => {
+        file.path = __dirname + '/uploads/' + file.name
+    })
+
+    form.on('file', (name, file) => {
+        console.log('Uploaded ' + file.name)
+    })
+
+    res.sendFile(__dirname + '/index.html')
+})
+app.listen(3000)
