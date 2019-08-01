7250129c718efdc419476fc51a9680a1a284be7c
Ryan <RyanCTaylor95@gmail.com>
2019-08-01 11:53:45 +1000

    Forgot to commit last time

--- W4/Angular_1/my-app/src/app/app-routing.module.ts
+++ W4/Angular_1/my-app/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { my-page } from './my-page/my-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'my-page', component: my-page}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- W4/Angular_1/my-app/src/app/app.module.ts
+++ W4/Angular_1/my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyPageComponent } from './my-page/my-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPageComponent
   ],
   imports: [
     BrowserModule,
